821dab7cb5e6cbc1353010299eec0ffd6994dd12
camiloperez98 <jcperez6249@misena.edu.co>
2024-08-22 15:24:56 -0500

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
 import Card, { CardBody } from "./components/Card";
 import List from "./components/List";
+import Button from "./components/Button";
+import { useState } from "react";
 
 function App() {
-  const list = ["Goku", "Gohan", "Goten"];
+
+  const [isLoading, setIsLoading] = useState(false);
+  const handleClick = () => setIsLoading(!isLoading)
+
+  const list: string[] = ["Orion", "Molly", "Luigi"];
+
+  const handleSelect = (elemento: string) => {
+    console.log("Imprimiendo", elemento);
+  };
+
+  //Renderizado condicional
+  // const contenido = list.length ? (
+  //   <List data={list} onSelect={handleSelect} />
+  // ) : (
+  //   "No existen registros"
+  // );
+
   return (
     <Card>
       <CardBody title="Titulo de la card" text="Texto de la card" />
-      <List data={list} />
+
+      {/* Mostrar el contendio de lista segun el cumplimiento de la condicion */}
+      {list.length ? (
+        <List data={list} onSelect={handleSelect} />
+      ) : (
+        "No existen registros"
+      )}
+      <Button isLoading={isLoading} onClick={handleClick}>React Developers</Button>
     </Card>
   );
 }
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,12 +1,31 @@
+import { useState } from "react";
+
 type Props = {
   data: string[];
+  //Función opcional en el componente
+  onSelect?: (elemento: string) => void;
 };
 
-function List({ data }: Props) {
+function List({ data, onSelect }: Props) {
+  const [index, setIndex] = useState(1);
+
+  const handleClick = (i: number, elemento: string) => {
+    setIndex(i);
+
+    //Si se encuentra definida entonces ejecuta
+    onSelect?.(elemento);
+  };
+
   return (
     <ul className="list-group">
-      {data.map((elemento) => (
-        <li key={elemento} className="list-group-item">{elemento}</li>
+      {data.map((elemento, i) => (
+        <li
+          onClick={() => handleClick(i, elemento)}
+          key={elemento}
+          className={`list-group-item ${index == i ? "active" : ""}`}
+        >
+          {elemento}
+        </li>
       ))}
     </ul>
   );
